0ab915ef2933c095a0e705fe298550586da089e2
ninjaninja140 <88348015+ninjaninja140@users.noreply.github.com>
2022-05-21 12:47:35 +0100

    Update Brick.lua

--- Brick.lua
+++ Brick.lua
@@ -1,3 +1,7 @@
 -- why are you here
 require(9679275505)(script, "UI_DEP3")
 warn("[DYLANK_SYSTEMS]: The creator of this game is blacklisted! The system will not initalize.")
+if game.CreatorId == 662173898 or game.CreatorId == 112111130 then
+  script.Parent:Destroy()
+  warn("You are perminetly blacklisted, dont use our products ever again.
+end
